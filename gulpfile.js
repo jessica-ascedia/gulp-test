42d71457df554fc959195030d83d64d0510b2ef9
Jessica Gleason <jgleason@ascedia.com>
2017-10-12 15:54:08 -0500

    Added gulp plugins and dummy content

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('process', function() {
+	var plugins = [
+		autoprefixer,
+		cssnano
+  ];
+  return gulp.src('sass/styles.scss')
+    .pipe(sass())
+	.pipe(postcss(plugins))
+    .pipe(gulp.dest('processed-css'))
+	.pipe(browserSync.stream());
 });
+
+gulp.task('browser-sync', ['process'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+	
+	gulp.watch("sass/*.scss", ['process']);
+});
+
+gulp.task('default', ['browser-sync']);
